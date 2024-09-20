764de850acd0b2fd8c7f0bae0b1ce83c10d8e01a
Reyem501 <59642674+Reyem501@users.noreply.github.com>
2024-09-20 15:03:28 +0200

    Add files via upload

---
+++ CalculatriceMVC/src/main/java/fr/demo/CalculatriceModel.java
@@ -0,0 +1,39 @@
+package fr.demo;
+
+public class CalculatriceModel {
+
+    private double resultat;
+
+    // Addition
+    public double addition(double a, double b) {
+        return a + b;
+    }
+
+    // Soustraction
+    public double soustraction(double a, double b) {
+        return a - b;
+    }
+
+    // Multiplication
+    public double multiplication(double a, double b) {
+        return a * b;
+    }
+
+    // Division
+    public double division(double a, double b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException("Erreur : Division par zéro");
+        }
+        return a / b;
+    }
+
+    // Getter
+    public double getResultat() {
+        return resultat;
+    }
+
+    // Setter
+    public void setResultat(double resultat) {
+        this.resultat = resultat;
+    }
+}
