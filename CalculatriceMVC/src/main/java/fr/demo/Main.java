f3c6e4a8e01dd2dd3890d42881d31220196d652b
Reyem501 <59642674+Reyem501@users.noreply.github.com>
2024-09-25 18:03:49 +0200

    Update Main.java
    Modification Main file for run

--- CalculatriceMVC/src/main/java/fr/demo/Main.java
+++ CalculatriceMVC/src/main/java/fr/demo/Main.java
@@ -2,6 +2,10 @@ package fr.demo;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        CalculatriceModel model = new CalculatriceModel();
+        CalculatriceView view = new CalculatriceView();
+        CalculatriceController controller = new CalculatriceController(model, view);
+        view.setVisible(true);
     }
 }
