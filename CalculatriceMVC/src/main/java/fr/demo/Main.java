6ea75c5ed95bcc2fe30e0d80ef3e96f30bf8d7c0
Reyem501 <59642674+Reyem501@users.noreply.github.com>
2024-09-28 16:49:22 +0200

    Main pour javaFX

--- CalculatriceMVC/src/main/java/fr/demo/Main.java
+++ CalculatriceMVC/src/main/java/fr/demo/Main.java
@@ -1,11 +1,23 @@
+/*
 package fr.demo;
 
 public class Main {
     public static void main(String[] args) {
 
         CalculatriceModel model = new CalculatriceModel();
-        CalculatriceView view = new CalculatriceView();
+        CalculatriceViewSwing view = new CalculatriceViewSwing();
         CalculatriceController controller = new CalculatriceController(model, view);
         view.setVisible(true);
+    }
+}
+*/
+
+package fr.demo;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(CalculatriceViewJavaFX.class, args);
     }
 }
