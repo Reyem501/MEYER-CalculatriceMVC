66b90b1a84b4867308958c228e68558a73d17298
Reyem501 <59642674+Reyem501@users.noreply.github.com>
2024-09-28 18:11:06 +0200

    changement pour choisir entre swing et javaFX

--- CalculatriceMVC/src/main/java/fr/demo/Main.java
+++ CalculatriceMVC/src/main/java/fr/demo/Main.java
@@ -1,23 +1,34 @@
-/*
-package fr.demo;
-
-public class Main {
-    public static void main(String[] args) {
-
-        CalculatriceModel model = new CalculatriceModel();
-        CalculatriceViewSwing view = new CalculatriceViewSwing();
-        CalculatriceController controller = new CalculatriceController(model, view);
-        view.setVisible(true);
-    }
-}
-*/
-
 package fr.demo;
 
 import javafx.application.Application;
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        Application.launch(CalculatriceViewJavaFX.class, args);
+        // Afficher une boîte de dialogue pour permettre à l'utilisateur de choisir entre Swing et JavaFX
+        Object[] options = {"JavaFX", "Swing"};
+        int choix = JOptionPane.showOptionDialog(
+                null,
+                "Choisissez l'interface à utiliser :",
+                "Sélection de l'interface",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                options,
+                options[0]
+        );
+
+        if (choix == 0) {
+            // L'utilisateur choisit JavaFX
+            Application.launch(CalculatriceViewJavaFX.class, args);
+        } else if (choix == 1) {
+            // L'utilisateur choisit Swing
+            CalculatriceModel model = new CalculatriceModel();
+            CalculatriceViewSwing view = new CalculatriceViewSwing();
+            SwingController controller = new SwingController(model, view);
+            view.setVisible(true);
+        } else {
+            System.exit(0);
+        }
     }
 }
