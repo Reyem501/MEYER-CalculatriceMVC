e0e69b85119fb8e8311e1b813ff2bbf85d4379cd
Reyem501 <59642674+Reyem501@users.noreply.github.com>
2024-09-28 16:48:57 +0200

    Controller pour javaFX

--- CalculatriceMVC/src/main/java/fr/demo/CalculatriceController.java
+++ CalculatriceMVC/src/main/java/fr/demo/CalculatriceController.java
@@ -1,3 +1,4 @@
+/*
 package fr.demo;
 
 import javax.swing.*;
@@ -6,11 +7,11 @@ import java.awt.event.ActionListener;
 
 public class CalculatriceController {
 
-    private final CalculatriceView view;
+    private final CalculatriceViewSwing view;
     private String operateur = "";
     private double firstNb = 0;
 
-    public CalculatriceController(CalculatriceModel model, CalculatriceView view) {
+    public CalculatriceController(CalculatriceModel model, CalculatriceViewSwing view) {
         this.view = view;
 
         // Ecouteurs sur les boutons de chiffre
@@ -82,6 +83,96 @@ public class CalculatriceController {
 
         @Override
         public void actionPerformed(ActionEvent e) {
+            firstNb = Double.parseDouble(view.getEcran().getText());
+            operateur = operateurSelectionne;
+            view.getEcran().setText("");
+        }
+    }
+}
+*/
+
+package fr.demo;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+
+public class CalculatriceController {
+
+    private final CalculatriceViewJavaFX view;
+    private String operateur = "";
+    private double firstNb = 0;
+
+    public CalculatriceController(CalculatriceModel model, CalculatriceViewJavaFX view) {
+        this.view = view;
+
+        // Écouteurs pour les boutons de chiffres
+        for (Button bouton : view.getChiffres()) {
+            bouton.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    String chiffre = bouton.getText();
+                    view.getEcran().setText(view.getEcran().getText() + chiffre);
+                }
+            });
+        }
+
+        // Écouteurs pour les boutons d'opération
+        view.getAddition().setOnAction(new OperateurListener("+"));
+        view.getSoustraction().setOnAction(new OperateurListener("-"));
+        view.getMultiplication().setOnAction(new OperateurListener("*"));
+        view.getDivision().setOnAction(new OperateurListener("/"));
+
+        // Écouteur pour le bouton égal
+        view.getEgal().setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                double secondNb = Double.parseDouble(view.getEcran().getText());
+                try {
+                    switch (operateur) {
+                        case "+":
+                            model.setResultat(model.addition(firstNb, secondNb));
+                            break;
+                        case "-":
+                            model.setResultat(model.soustraction(firstNb, secondNb));
+                            break;
+                        case "*":
+                            model.setResultat(model.multiplication(firstNb, secondNb));
+                            break;
+                        case "/":
+                            model.setResultat(model.division(firstNb, secondNb));
+                            break;
+                    }
+                    double resultat = model.getResultat();
+                    if (resultat == (int) resultat) {
+                        view.getEcran().setText(String.valueOf((int) resultat));
+                    } else {
+                        view.getEcran().setText(String.valueOf(resultat));
+                    }
+                } catch (ArithmeticException ex) {
+                    view.getEcran().setText("Erreur : Division par zéro");
+                }
+            }
+        });
+
+        // Écouteur pour le bouton clear
+        view.getClear().setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                view.getEcran().setText("");
+            }
+        });
+    }
+
+    private class OperateurListener implements EventHandler<ActionEvent> {
+        private final String operateurSelectionne;
+
+        public OperateurListener(String operateur) {
+            this.operateurSelectionne = operateur;
+        }
+
+        @Override
+        public void handle(ActionEvent event) {
             firstNb = Double.parseDouble(view.getEcran().getText());
             operateur = operateurSelectionne;
             view.getEcran().setText("");
