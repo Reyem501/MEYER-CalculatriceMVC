5a07d7c94921b15a12b879794fe3828d217c862f
Reyem501 <59642674+Reyem501@users.noreply.github.com>
2024-10-07 17:45:26 +0200

    ajout listener pi + arrondi interface

--- CalculatriceMVC/src/main/java/fr/demo/CalculatriceViewInterface.java
+++ CalculatriceMVC/src/main/java/fr/demo/CalculatriceViewInterface.java
@@ -8,8 +8,10 @@ public interface CalculatriceViewInterface {
 
     void addChiffreListener(int chiffre, Runnable listener);
     void addOperationListener(String operation, Runnable listener);
+    void addPIListener(Runnable listener);
     void addEgalListener(Runnable listener);
     void addClearListener(Runnable listener);
     void addPointListener(Runnable listener);
     void addSigneListener(Runnable listener);
+    void addArrondirListener(Runnable listener);
 }
